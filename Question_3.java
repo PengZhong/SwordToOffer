9fa8bfa7076b4e66620e6892eac656ac123b4e6d
Hound <pengmany@outlook.com>
2016-09-29 10:36:23 +0800

    add null judge

--- Question_3.java
+++ Question_3.java
@@ -5,9 +5,11 @@ public class Question_3 {
 	// 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
 	public static boolean solution(int[][] arr, int key) {
+		if (arr == null) {
+			return false;
+		}
 		int rows = arr.length;
 		int cols = arr[0].length;
-		boolean found = false;
 		int row = 0;
 		int col = cols - 1;
 		while (row < rows && col >= 0) {
@@ -19,14 +21,16 @@ public class Question_3 {
 				col--;
 			}
 		}
-		return found;
+		return false;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] arr = { { 1, 2, 8, 9 }, { 2, 4, 9, 12 }, { 4, 7, 10, 13 }, { 6, 8, 11, 15 } };
-		boolean result = solution(arr, 3);
-		System.out.println(result);
+		int[][] arr2 = null;
+		System.out.println(solution(arr, 7));
+		System.out.println(solution(arr, 3));
+		System.out.println(solution(arr2, 3));
 	}
 
 }
