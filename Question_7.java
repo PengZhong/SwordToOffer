3767b7336a2895c92c8698ee025ab750fa711bb5
Hound <pengmany@outlook.com>
2016-09-30 08:35:28 +0800

    add annotation

--- Question_7.java
+++ Question_7.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class Question_7 {
-	
+	// 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
 	Stack<Integer> stack1 = new Stack<Integer>();
 	Stack<Integer> stack2 = new Stack<Integer>();
 	
